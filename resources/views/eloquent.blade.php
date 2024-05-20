5eaa93a9b972a0889359a1584f8f1cd1d7e19cb0
Ryandra25 <118356454+Ryandra25@users.noreply.github.com>
2024-05-20 15:03:51 +0700

    Add files via upload

---
+++ resources/views/eloquent.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <style type="text/css">
+        body {
+            padding-top: 50px;
+        }
+    </style>
+</head>
+
+
+<body class="container">
+    <div class="col-sm-8 col-sm-offset-2">
+        @foreach ($mahasiswa as $temp)
+            <h3> <small>[{{$temp->nama}}]</small></h3>
+            <h5>Hobi :
+                @foreach ($temp->hobi as $result)
+                    <small>{{$result->hobi}}</small>
+                @endforeach
+            </h5>
+            <h4>
+                <li>Nama Wali : <strong>{{ $temp->wali->nama}}</strong></li>
+            <li>Dosen Pembimbing : <strong>{{$temp->dosen->nama}}</strong></li>
+            </h4>
+            <hr />
+        @endforeach
+
+
+
+
+</body>
+</html>
